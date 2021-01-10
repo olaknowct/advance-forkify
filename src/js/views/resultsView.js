d6d441b42d5aa13b83c5ec0488e2c1bee134b639
Christopher Olano <olanochristopher@gmail.com>
2021-01-10 22:13:15 +0800

    Fix active recipe : class typo error

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -15,7 +15,7 @@ class ResultsView extends View {
     return `
     <li class="preview">
         <a class="preview__link ${
-          result.id === id ? 'preview_link--active' : ''
+          result.id === id ? 'preview__link--active' : ''
         }" href="#${result.id}">
             <figure class="preview__fig">
             <img src="${result.image}" alt="${result.title}" />
